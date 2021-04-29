d5b32bde724d4b6e58d82529bda764ca8106c5a2
Karsten Ohme <k_o_@users.sourceforge.net>
2021-04-29 05:25:43 +0200

    Formulae fixes

--- Formula/globalplatform.rb
+++ Formula/globalplatform.rb
@@ -3,16 +3,15 @@ require "open3"
 class Globalplatform < Formula
   desc "C library + command-line for Open- / GlobalPlatform smart cards"
   homepage "https://kaoh.github.io/globalplatform/"
-  url "https://github.com/kaoh/globalplatform.git", tag: "2.0.0"
-  #  version "2.0.0"
+  url "https://github.com/kaoh/globalplatform.git", tag: "2.1.0"
+  #  version "2.1.0"
   head "https://github.com/kaoh/globalplatform.git"
 
   bottle do
     root_url "https://dl.bintray.com/kaoh/bottles-globalplatform"
-    cellar :any
     rebuild 1
-    sha256 "298e8dbd8710cf3c25142446c49e37b6f16b36d97e0985e43a606ab084b5df50" => :high_sierra
-    sha256 "1194a95d357783b24851582c7c0392c6b699dabcf4fed473ad7df6aa78c69d89" => :x86_64_linux
+    sha256 cellar: :any, high_sierra:  "298e8dbd8710cf3c25142446c49e37b6f16b36d97e0985e43a606ab084b5df50"
+    sha256 cellar: :any, x86_64_linux: "1194a95d357783b24851582c7c0392c6b699dabcf4fed473ad7df6aa78c69d89"
   end
 
   depends_on "cmake" => :build
