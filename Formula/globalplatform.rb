db6c6bdcebb63f280c12dc048b18639665bbb94e
Karsten Ohme <k_o_@users.sourceforge.net>
2022-08-09 15:33:12 +0200

    updated linux image

--- Formula/globalplatform.rb
+++ Formula/globalplatform.rb
@@ -9,7 +9,7 @@ class Globalplatform < Formula
   bottle do
     root_url "https://github.com/kaoh/homebrew-globalplatform/releases/download/2.3.1"
     sha256 cellar: :any,                 catalina:     "03af7fcd4393c3460d61aac9d756bd99ccb137252805e5e6bb2bd2971d9a84e5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "e2611aeca3d02f3052c1715e8e71ff9c9764be95eb9c0d8f3fcf65cf0424bb95"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b03e31e9749c7051bdcb7663d4137bbb0997807d4edb255d8042df694b1760f5"
   end
 
   depends_on "cmake" => :build
